64a33c87ffe5c5e3f67ea6ed7c16dc5e612e3e45
Abdulkadir KOCER <akocerweb@hotmail.com>
2024-02-27 14:47:29 +0300

    Add project files.

---
+++ uyg01/Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uyg01.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("data source=.;initial catalog=CBS01DB;user id=sa; password=123;TrustServerCertificate=True");
+        }
+    }
+}
