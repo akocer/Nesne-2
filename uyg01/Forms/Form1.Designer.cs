ad1226a685680cacd2b6836b6fe6d485dd4fb3d1
Abdulkadir KOCER <akocerweb@hotmail.com>
2024-02-27 16:34:15 +0300

    update

--- uyg01/Forms/Form1.Designer.cs
+++ uyg01/Forms/Form1.Designer.cs
@@ -28,18 +28,188 @@
         /// </summary>
         private void InitializeComponent()
         {
+            dgProducts = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            label1 = new Label();
+            txtId = new TextBox();
+            txtName = new TextBox();
+            label2 = new Label();
+            txtPrice = new TextBox();
+            label3 = new Label();
+            btnSave = new Button();
+            btnUpdate = new Button();
+            btnClose = new Button();
+            btnDelete = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgProducts).BeginInit();
             SuspendLayout();
+            // 
+            // dgProducts
+            // 
+            dgProducts.AllowUserToAddRows = false;
+            dgProducts.AllowUserToDeleteRows = false;
+            dgProducts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgProducts.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3 });
+            dgProducts.Location = new Point(47, 39);
+            dgProducts.Name = "dgProducts";
+            dgProducts.ReadOnly = true;
+            dgProducts.RowHeadersWidth = 62;
+            dgProducts.RowTemplate.Height = 33;
+            dgProducts.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgProducts.Size = new Size(635, 401);
+            dgProducts.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "KN";
+            Column1.MinimumWidth = 8;
+            Column1.Name = "Column1";
+            Column1.ReadOnly = true;
+            Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Adı";
+            Column2.MinimumWidth = 8;
+            Column2.Name = "Column2";
+            Column2.ReadOnly = true;
+            Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Fiyatı";
+            Column3.MinimumWidth = 8;
+            Column3.Name = "Column3";
+            Column3.ReadOnly = true;
+            Column3.Width = 150;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(722, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(86, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Kayıt No";
+            // 
+            // txtId
+            // 
+            txtId.Location = new Point(859, 35);
+            txtId.Name = "txtId";
+            txtId.ReadOnly = true;
+            txtId.Size = new Size(68, 31);
+            txtId.TabIndex = 2;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(859, 72);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(150, 31);
+            txtName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(722, 78);
+            label2.Name = "label2";
+            label2.Size = new Size(41, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Adı";
+            // 
+            // txtPrice
+            // 
+            txtPrice.Location = new Point(859, 109);
+            txtPrice.Name = "txtPrice";
+            txtPrice.Size = new Size(150, 31);
+            txtPrice.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(722, 115);
+            label3.Name = "label3";
+            label3.Size = new Size(58, 25);
+            label3.TabIndex = 5;
+            label3.Text = "Fiyatı";
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(722, 173);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(178, 47);
+            btnSave.TabIndex = 7;
+            btnSave.Text = "Kaydet";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(906, 173);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(178, 47);
+            btnUpdate.TabIndex = 7;
+            btnUpdate.Text = "Düzenle";
+            btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            btnClose.Location = new Point(906, 226);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(178, 47);
+            btnClose.TabIndex = 8;
+            btnClose.Text = "Kapat";
+            btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(722, 226);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(178, 47);
+            btnDelete.TabIndex = 9;
+            btnDelete.Text = "Sil";
+            btnDelete.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(11F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(927, 551);
+            ClientSize = new Size(1143, 462);
+            Controls.Add(btnClose);
+            Controls.Add(btnDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnSave);
+            Controls.Add(txtPrice);
+            Controls.Add(label3);
+            Controls.Add(txtName);
+            Controls.Add(label2);
+            Controls.Add(txtId);
+            Controls.Add(label1);
+            Controls.Add(dgProducts);
+            Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dgProducts).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dgProducts;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private Label label1;
+        private TextBox txtId;
+        private TextBox txtName;
+        private Label label2;
+        private TextBox txtPrice;
+        private Label label3;
+        private Button btnSave;
+        private Button btnUpdate;
+        private Button btnClose;
+        private Button btnDelete;
     }
 }
--- uyg01/Forms/Form1.cs
+++ uyg01/Forms/Form1.cs
@@ -1,3 +1,5 @@
+using uyg01.Models;
+
 namespace uyg01
 {
     public partial class Form1 : Form
@@ -5,6 +7,41 @@ namespace uyg01
         public Form1()
         {
             InitializeComponent();
+        }
+
+        AppDbContext db = new AppDbContext();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetProductList();
+        }
+
+        void GetProductList()
+        {
+            dgProducts.Rows.Clear();
+            var products = db.Products.ToList();
+            foreach (var product in products)
+            {
+                dgProducts.Rows.Add(product.Id, product.Name, product.Price);
+            }
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text == "" || txtPrice.Text == "")
+            {
+                MessageBox.Show("Tüm Alanlarý Doldurunuz!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var product = new Product()
+            {
+                Name = txtName.Text,
+                Price = Convert.ToDouble(txtPrice.Text)
+
+            };
+            db.Products.Add(product);
+            db.SaveChanges();
+            GetProductList();
+            MessageBox.Show("Kayýt Eklendi", "Tamam", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
