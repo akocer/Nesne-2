ba7c628a1f56d24ae768118bb252bbca30dcd0e3
Abdulkadir KOCER <akocerweb@hotmail.com>
2024-03-12 14:20:29 +0300

    uyg02

---
+++ uyg02/Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uyg02.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("data source=.;initial catalog=CBS02DB;user id=sa; password=123;TrustServerCertificate=True");
+        }
+    }
+}
