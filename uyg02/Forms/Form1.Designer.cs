6c3270ce4756f7054f43f5dbc33ad16b6c7b35dc
Abdulkadir KOCER <akocerweb@hotmail.com>
2024-03-12 15:56:45 +0300

    update

--- uyg02/Forms/Form1.Designer.cs
+++ uyg02/Forms/Form1.Designer.cs
@@ -29,34 +29,155 @@
         private void InitializeComponent()
         {
             dgCategories = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            label1 = new Label();
+            txtId = new TextBox();
+            txtName = new TextBox();
+            label2 = new Label();
+            btnSave = new Button();
+            btnUpdate = new Button();
+            btnClear = new Button();
+            btnDelete = new Button();
             ((System.ComponentModel.ISupportInitialize)dgCategories).BeginInit();
             SuspendLayout();
             // 
             // dgCategories
             // 
+            dgCategories.AllowUserToAddRows = false;
+            dgCategories.AllowUserToDeleteRows = false;
             dgCategories.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgCategories.Location = new Point(41, 35);
+            dgCategories.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2 });
+            dgCategories.Location = new Point(45, 35);
             dgCategories.Name = "dgCategories";
+            dgCategories.ReadOnly = true;
             dgCategories.RowHeadersWidth = 62;
             dgCategories.RowTemplate.Height = 33;
-            dgCategories.Size = new Size(619, 307);
+            dgCategories.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgCategories.Size = new Size(361, 290);
             dgCategories.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "Id";
+            Column1.MinimumWidth = 8;
+            Column1.Name = "Column1";
+            Column1.ReadOnly = true;
+            Column1.Width = 50;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Adı";
+            Column2.MinimumWidth = 8;
+            Column2.Name = "Column2";
+            Column2.ReadOnly = true;
+            Column2.Width = 150;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(44, 356);
+            label1.Name = "label1";
+            label1.Size = new Size(29, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Id";
+            // 
+            // txtId
+            // 
+            txtId.Location = new Point(109, 350);
+            txtId.Name = "txtId";
+            txtId.ReadOnly = true;
+            txtId.Size = new Size(98, 31);
+            txtId.TabIndex = 2;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(109, 387);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(297, 31);
+            txtName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(44, 393);
+            label2.Name = "label2";
+            label2.Size = new Size(41, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Adı";
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(109, 433);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(143, 34);
+            btnSave.TabIndex = 5;
+            btnSave.Text = "Kaydet";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(263, 433);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(143, 34);
+            btnUpdate.TabIndex = 5;
+            btnUpdate.Text = "Düzenle";
+            btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(263, 473);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(143, 34);
+            btnClear.TabIndex = 6;
+            btnClear.Text = "Temizle";
+            btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(109, 473);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(143, 34);
+            btnDelete.TabIndex = 7;
+            btnDelete.Text = "Sil";
+            btnDelete.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(11F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(940, 597);
+            Controls.Add(btnClear);
+            Controls.Add(btnDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnSave);
+            Controls.Add(txtName);
+            Controls.Add(label2);
+            Controls.Add(txtId);
+            Controls.Add(label1);
             Controls.Add(dgCategories);
+            Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)dgCategories).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dgCategories;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private Label label1;
+        private TextBox txtId;
+        private TextBox txtName;
+        private Label label2;
+        private Button btnSave;
+        private Button btnUpdate;
+        private Button btnClear;
+        private Button btnDelete;
     }
 }
--- uyg02/Forms/Form1.cs
+++ uyg02/Forms/Form1.cs
@@ -11,7 +11,39 @@ namespace uyg02
         AppDbContext db = new AppDbContext();
         private void Form1_Load(object sender, EventArgs e)
         {
-            dgCategories.DataSource = db.Categories.ToList();
+            CategoryList();
+        }
+
+        void CategoryList()
+        {
+            var categories = db.Categories.ToList();
+
+            foreach (var category in categories)
+            {
+                dgCategories.Rows.Add(category.Id, category.Name);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text == "")
+            {
+                MessageBox.Show("Kategori Adý Giriniz!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (db.Categories.Count(c => c.Name == txtName.Text) > 0)
+            {
+                MessageBox.Show("Girilen Kategori Adý Kayýtlýdýr!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            var category = new Category();
+            category.Name = txtName.Text;
+            db.Categories.Add(category);
+            db.SaveChanges();
+            CategoryList();
+            MessageBox.Show("Kategori Eklendi", "Tamam", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
         }
     }
 }
