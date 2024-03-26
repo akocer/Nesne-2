20864957c85b6aa65f6516793b163490444fa872
Abdulkadir KOCER <akocerweb@hotmail.com>
2024-03-26 14:21:40 +0300

    uyg03

---
+++ uyg03/Models/Lesson.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uyg03.Models
+{
+    public class Lesson
+    {
+        public int Id { get; set; }
+
+        [StringLength(10)]
+        public string Code { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public int Credit { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+    }
+}
---
+++ uyg03/Models/Register.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uyg03.Models
+{
+    public class Register
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public int LessonId { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+    }
+}
