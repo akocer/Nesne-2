7d29254215f432458f6a48db8dd8dc31776816bb
Abdulkadir KOCER <akocerweb@hotmail.com>
2024-05-07 14:17:08 +0300

    update

---
+++ uyg04/Models/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uyg04.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Work> Works { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("data source=.;initial catalog=CBS04DB;user id=sa; password=123;TrustServerCertificate=True");
+        }
+
+    }
+}
